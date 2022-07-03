6f32ed6ecd39945aa4a50a835bc6bc674155a0cd
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-03 11:44:59 -0700

    102taskv

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,23 +7,30 @@
  */
 int main(void)
 {
-	int digits_1, digits_2;
+	int num1, num2, num3, num4, num5, num6;
 
-	for (digit_1 = 0; digit_1 <= 98; digit_1++)
+	for (num5 = 0; num5 < 100; num5++)
 	{
-	for (digit_2 = digit_1 + 1; digit_2 <= 99; digit_2++)
+	num1 = num5 / 10;
+	num2 = num5 % 10;
+	for (num6 = 0; num6 < 100; num6++)
 	{
-	putchar((digit_1 / 10) + '0');
-	putchar((digit_1 % 10) + '0');
-	putchar(' ');
-	putchar((digit_2 / 10) + '0');
-	putchar((digit_2 % 10) + '0');
-	if (digit_1 == 98 && digit_2 == 99)
-	continue;
-	putchar(',');
-	putchar(' ');
+	num3 = num6 / 10;
+	num4 = num6 % 10;
+	if (num1 < num3 || (num1 == num3 && num2 < num4))
+	{
+	putchar(num1 + '0');
+	putchar(num2 + '0');
+	putchar(32);
+	putchar(num3 + '0');
+	putchar(num4 + '0');
+	{
+	putchar(44);
+	putchar(32);
 	}
 	}
-	putchar('\n');
+	}
+	}
+	putchar(10);
 	return (0);
 }
