81eb6e9acc4515fce4c91578fa021975288f5ba3
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-03 14:04:11 -0700

    101 tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 #include <unistd.h>
 /**
  * main-main block
  *
- * Return: 9
+ * Return: Always 0 success
  */
 int main(void)
 {
-	int s;
 
-	s = strlen("and that piece of art is useful\"-Dora Korpa,2015-10-19\n");
-	write(2, "and that piece of art is useful\"-Dora Korpa,2015-10-19\n", s);
-	return (9);
+	write(2, "and that piece of art is useful\" - Dora Korpa,2015-10-19\n", 59);
+
+	return (1);
 }
