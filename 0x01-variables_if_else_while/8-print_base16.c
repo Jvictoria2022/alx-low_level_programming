a9b2ca2579689cf9b22d76f5b09c9bec28a39fa4
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-02 20:25:12 -0700

    8taskv0

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	char lower;
 
 	for (digit = 0; digit < 10; digit++)
-	putchar((digit % 10)+0);
+	putchar((digit % 10) + '0');
 	for (lower = 'a'; lower <= 'f'; lower++)
 	putchar(lower);
 	putchar('\n');
