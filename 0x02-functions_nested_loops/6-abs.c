28840e9e0965fbe2efe6649b5986d08c611df0ca
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-05 16:36:43 -0700

    task6abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _abs - Entry point
- *@int: computes the absolute value of an integer
+ *@t: computes the absolute value of an integer
  *
- *
+ *Return: 1 if c is  is a letter, lowercase or uppercase,otherwise 0
  */
-int _abs(int n)
+int _abs(int t)
 {
-	if (n >= 0);
+	if (n >= 0)
 	return (n);
 	else
 		return (-n);
