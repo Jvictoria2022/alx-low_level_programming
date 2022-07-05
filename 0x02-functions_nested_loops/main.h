3fe8727034ebafdd7254f72031b16993407710ef
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-05 16:29:26 -0700

    tasksabs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,10 +8,10 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+int _abs(int t);
+int print_last_digit(int p);
 void jack_bauer(void);
-int add(int, int);
+int add(int e, int f);
 void print_to_98 (int n);
 void print_times_table(int n);
 #endif /* MAIN_H */
