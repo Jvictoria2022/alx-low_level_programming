5ba7f6deb27b87466dabb5d274bb7bec4a21291f
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-05 14:32:13 -0700

    alpha10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,16 @@
 void print_alphabet_x10(void)
 {
 	char tAlpha;
-	int product;
+	int product = 0;
+
+	while (product++ <= 9)
+	{
 
 	for (tAlpha = 97; tAlpha = 122; tAlpha++)
-		product = 10 * tAlpha;
+
 	_putchar(tAlpha);
 
 	_putchar('\n');
+
+	}
 }
