3a270c59566615fb4f5d73336ed2af2fc16b0773
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-07 12:58:55 -0700

    0isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,14 @@
-
+#include "main.h"
+/**
+ *_isupper - Entry point,uppercase letters
+ *@c: checks characters
+ *
+ *Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
