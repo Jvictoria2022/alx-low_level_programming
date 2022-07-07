204c8a3a4c526b32795fdd252dadd2762a6f0526
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-07 15:00:32 -0700

    04print most

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print _numbers - This prints outs 1,2...10
+ *@void: from 0 to
+ * Return: 1 0r 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
+	
