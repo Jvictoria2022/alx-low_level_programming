d1eb33c58571c104c3e4c9931ce6abd4e92939bc
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-07 04:28:46 -0700

    100timestask12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ *print_times_table - Entry point, prints the n times table, starting with 0
+ *@n:  is greater than 15 < 0
+ *
+ *
+ */
+void print_times_table(int n)
+{
+	int s = 0, inc, t;
+
+	if (n < 0 || n > 15)
+		return;
+	while (s <= n)
+	{
+		for (t = 0; t <= n; t++)
+		{
+			inc = s * t;
+			if (t == 0)
+				_putchar('0' + inc);
+			else if (inc < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + inc);
+			}
+			else if (inc < 100)
+			{
+				_putchar('0' + inc / 100);
+				_putchar('0' + (inc - 100) / 10);
+				_putchar('0' + inc % 10);
+				}
+				if (t < n)
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
+				}
+				_putchar('\n');
+				s++;
+				}
+				}
