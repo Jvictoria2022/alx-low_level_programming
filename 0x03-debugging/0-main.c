495ff56c8caecf63d1d55b3d4359d6f8f347c50c
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-08 05:45:41 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- *main - Entry point,tests function to print +ve or -ve
+ *main - Entry point,tests function to print positive or negative
  *
  *
  *Return: 0
  */
+
 int main(void)
 {
 	int n;
 
 	n = 0;
-	if (n < 0 && n > 0);
+	positive_or_negative(n);
 	return (0);
 }
