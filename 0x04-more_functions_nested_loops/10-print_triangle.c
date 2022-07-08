befe63f0af8671ba33d7d061c4871d17b02ab1f4
Jvictoria2022 <jobosonogochukwu@gmail.com>
2022-07-08 04:13:56 -0700

    0main

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *Print_triangle - Entry point
+ *prints a trianle shape
+ *
+ *Return: void
+ */
+void print_triangle(int size)
+{
+	int row, lashes, gaps;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (gaps = size - row; gaps >= 1; gaps--)
+			{
+				_putchar(' ');
+			}
+
+				for (lashes =1; lashes <= row; lashes++)
+				{
+					_putchar("#");
+				}
+				_putchar('\n');
+		}
+}	}
