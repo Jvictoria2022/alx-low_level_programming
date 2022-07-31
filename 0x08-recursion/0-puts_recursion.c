053b1874df1c3e8a363080f86f602f87858ac5a9
Jvictoria2022 <jobosonauguster@yahoo.com>
2022-07-31 18:16:40 +0100

    0-puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - @s:prints a string, followed by a new line 
+ *@s: the string printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	_puts_recursion("Doing hard things");
+}
